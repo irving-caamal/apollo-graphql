a6030b4e510b91a6202cabff71638521a1bf0904
irvv17 <irvv.17@gmail.com>
2022-02-09 22:44:37 -0500

    Use case for EnumTypes

--- basic-example/index.js
+++ basic-example/index.js
@@ -62,6 +62,10 @@ const persons =[{
     "name": "Mavra Sijmons"
   }]
 const typeDefs = gql`
+    enum YesNo{
+      YES
+      NO
+    }
     type Address {
         street: String
         city: String
@@ -77,7 +81,7 @@ const typeDefs = gql`
     }
     type Query {
         personCount: Int!
-        allPersons: [Person]!
+        allPersons(phone: YesNo): [Person]!
         findPerson(name: String!): Person
     }
     type Mutation {
@@ -92,7 +96,13 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if (!args.phone) return persons
+      const byPhone = person => args.phone === 'YES' ?
+      person.phone : !person.phone
+
+      return persons.filter(byPhone)
+    },
     findPerson: (root, args ) => {
       const { name } = args
       return persons.find(person => person.name === name);
