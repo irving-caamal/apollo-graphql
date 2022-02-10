b452e86d76848283875ad50c2d832cb21bcdc921
irvv17 <irvv.17@gmail.com>
2022-02-10 14:01:59 -0500

    feat: add more queries

--- ecommerce-app/index.ts
+++ ecommerce-app/index.ts
@@ -1,15 +1,44 @@
 import { ApolloServer, gql } from "apollo-server";
+import { IResolvers } from "@graphql-tools/utils";
+import * as data from './mockdata.json';
 
 const typeDefs = gql`
+    type Product {
+        id: ID!
+        name: String!
+        description: String!
+        quantity: Int!
+        price: Float!
+        onSale: Boolean!
+        image: String!
+    }
+    type Category {
+        id: ID!
+        name: String!
+    }
     type Query {
-        hello: String
+        products: [Product!]!
+        product(id: ID!): Product
+        categories: [Category!]!
+        category(id: ID!): Category
     }
 `;
-const resolvers = {
+const resolvers: IResolvers = {
     Query: {
-        hello: () => "Hello world!"
+        products: () => data.products,
+        product: (parent,args, context) => {
+            return data.products.find(product => product.id === args.id);
+        },
+        categories: (parent, args, context) => {
+            return data.categories; 
+        },
+        category: (parent, args, context) => {
+            const { id } = args;
+            return data.categories.find(category => category.id === id);
+        }
     }
 };
+
 const server = new ApolloServer({
     typeDefs,
     resolvers
