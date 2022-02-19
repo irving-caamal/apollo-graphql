618db734f9130c1dbf30eef52ad0835403613005
irvv17 <irvv.17@gmail.com>
2022-02-19 15:26:20 -0500

    feat: initial config of the first mutation using prisma client

--- social-media-app/src/index.ts
+++ social-media-app/src/index.ts
@@ -1,14 +1,22 @@
 import { ApolloServer } from "apollo-server";
 import { typeDefs } from "./schema";
 import { Query } from './resolvers';
+import { PrismaClient, Prisma } from '@prisma/client';
 
+const prisma = new PrismaClient();
+export interface Context {
+    prisma: PrismaClient<Prisma.PrismaClientOptions, never, Prisma.RejectOnNotFound | Prisma.RejectPerOperation | undefined>;
+}
 const resolvers = {
     Query
 }
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: {
+        prisma
+    }
 });
 
 server.listen().then(({ url }) => {
--- social-media-app/src/resolvers/mutation.ts
+++ social-media-app/src/resolvers/mutation.ts
@@ -1,5 +1,21 @@
-export const Mutation = {
-    postCreate: (parent, args, context, info) => {
-        //
+import { Context } from "../index"
+import { IResolvers } from '@graphql-tools/utils';
+
+interface PostCreateArgs {
+    title: string;
+    content: string;
+    published: boolean;
+}
+export const Mutation: IResolvers = {
+    postCreate: async (parent, args: PostCreateArgs, { prisma }: Context) => {
+        const { title, content, published } = args;
+        prisma.post.create({
+            data: {
+                title,
+                content,
+                published,
+                authorId: 1
+            }
+        });
     },
 }
