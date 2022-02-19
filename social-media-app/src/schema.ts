7e1c737fe5878623fb7d39c03c86edcb948ff2e4
irvv17 <irvv.17@gmail.com>
2022-02-19 14:57:09 -0500

    feat: defining our GraphQL schema

--- social-media-app/src/schema.ts
+++ social-media-app/src/schema.ts
@@ -4,6 +4,38 @@ const typeDefs = gql`
     type Query {
         hello: String
     }
+    type Post {
+        id: ID!
+        title: String!
+        content: String!
+        createdAt: String!
+        published: Boolean!
+        user: User!
+    }
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        profile: Profile!
+        post: [Post!]!
+    }
+    type Profile {
+        id: ID!
+        bio: String!
+        user: User!
+    }
+
+    type Mutation {
+        postCreate(title: String!, content: String!, published: Boolean!): PostPayload!
+        postUpdate(id: ID!, title: String!, content: String!, published: Boolean!): PostPayload!
+    }
+    type PostPayload {
+        userErrors: [UserError!]!
+        post: Post!
+    }
+    type UserError {
+        message: String!
+    }
 `;
 
 export {
