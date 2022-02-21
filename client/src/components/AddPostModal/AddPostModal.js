d3de50bf782e1895fdb5f17e51162a8b103e7265
irvv17 <irvv.17@gmail.com>
2022-02-20 19:41:41 -0500

    feat: add post depending on the authenticated user to reach our postCreate mutation

--- client/src/components/AddPostModal/AddPostModal.js
+++ client/src/components/AddPostModal/AddPostModal.js
@@ -1,17 +1,52 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
-
+import { useHistory } from "react-router";
+import { gql, useMutation } from "@apollo/client";
+const CREATE_POST = gql`
+  mutation CreatePost($title: String!, $content: String!, $published: Boolean!) {
+    postCreate(post: {
+      title: $title, content: $content, published: $published
+    }) {
+      userErrors {
+      message
+      }
+      post {
+        id
+        title
+      }
+    }
+  }
+`;
 export default function AddPostModal() {
   const [show, setShow] = useState(false);
-
+  const [error, setError] = useState(null);
+  const history = useHistory();
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
 
   const [content, setContent] = useState("");
   const [title, setTitle] = useState("");
-
-  const handleClick = () => {};
-
+  const [postCreate, { data, loading }] = useMutation(CREATE_POST);
+  const handleClick = () => {
+    if(!title || !content) {
+      setError("Title and content are required")
+    }
+    postCreate({
+      variables: { title, content, published: true },
+    });
+    handleClose();
+  };
+  useEffect(() => {
+    if (data) {
+      if(data.postCreate.userErrors.length) {
+        setError(data.signUp.userErrors[0].message)
+      }
+      if(data.postCreate.post) {
+        setError(null)
+        history.go(0)
+      }
+    }
+  }, [data])
   return (
     <>
       <Button variant="primary" onClick={handleShow}>
