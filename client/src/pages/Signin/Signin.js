5054e55271ee617fa86e0095a48e5e0a72f712d3
irvv17 <irvv.17@gmail.com>
2022-02-20 19:12:40 -0500

    feat: add signin/signup logic to consume our mutations using localStorage

--- client/src/pages/Signin/Signin.js
+++ client/src/pages/Signin/Signin.js
@@ -1,15 +1,44 @@
 import React, { useState, useEffect } from "react";
-
+import { useHistory } from "react-router-dom";
 import { Form } from "react-bootstrap";
 import Button from "@restart/ui/esm/Button";
+import { gql, useMutation } from "@apollo/client";
 
+const SIGN_IN = gql`
+  mutation SignIn($email: String!, $password: String!) {
+    signIn(credentials: { email: $email, password: $password }) {
+      userErrors {
+        message
+      }
+      token
+    }
+  }
+`;
 export default function Signin() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
-  const handleClick = () => {};
-
+  const history = useHistory();
+  const [signIn, { data, loading }] = useMutation(SIGN_IN);
   const [error, setError] = useState(null);
+  const handleClick = () => {
+    signIn({
+      variables: { email, password },
+    });
+    console.log({ data })
+  };
+  useEffect(() => {
+    if (data) {
+      if(data.signIn.userErrors.length) {
+        setError(data.signIn.userErrors[0].message)
+      }
+      if(data.signIn.token) {
+        setError(null)
+        localStorage.setItem("token", data.signIn.token)
+        history.push("/posts");
+      }
+    }
+    
+  }, [data])
 
   return (
     <div>
--- client/src/pages/Signup/Signup.js
+++ client/src/pages/Signup/Signup.js
@@ -1,17 +1,48 @@
 import Button from "@restart/ui/esm/Button";
 import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import { Form } from "react-bootstrap";
+import { gql, useMutation } from "@apollo/client";
 
+const SIGN_UP = gql`
+  mutation SignUp($email: String!, $password: String!, $name: String!, $bio: String) {
+    signUp(credentials: {
+      email: $email, password: $password
+    }, name: $name, bio: $bio) {
+      userErrors {
+        message
+      }
+      token
+    }
+  }
+`;
 export default function Signup() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [name, setName] = useState("");
   const [bio, setBio] = useState("");
-
-  const handleClick = () => {};
-
+  const [name, setName] = useState("");
+  const history = useHistory();
+  const [signUp, { data, loading }] = useMutation(SIGN_UP);
   const [error, setError] = useState(null);
-
+  const handleClick = () => {
+    signUp({
+      variables: { email, password, bio, name },
+    });
+    console.log({ data })
+  };
+  useEffect(() => {
+    if (data) {
+      if(data.signUp.userErrors.length) {
+        setError(data.signUp.userErrors[0].message)
+      }
+      if(data.signUp.token) {
+        setError(null)
+        localStorage.setItem("token", data.signUp.token)
+        history.push("/posts");
+      }
+    }
+    
+  }, [data])
   return (
     <div>
       <Form>
